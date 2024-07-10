97ddd82fbcaf1766ead89bbacd0ff5e71f68ff74
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-07-10 11:43:30 -0300

    feat: added Seed to DB

--- database/migrations/2024_06_21_161551_create_events_table.php
+++ database/migrations/2024_06_21_161551_create_events_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId(User::class);
+            $table->foreignIdFor(User::class);
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
 
-            $table->dateTime('start_date');
-            $table->dateTime('end_date');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
 
             $table->timestamps();
         });
